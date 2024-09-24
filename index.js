70791afaa9e5a6e6dc1fa6d9dc42a09523d34435
Harshit Bhargav <harshitbhargav627@gmail.com>
2024-09-24 12:25:29 +0530

    updated

--- index.js
+++ index.js
@@ -10,17 +10,24 @@ import applicationRoute from "./routes/applicationRoute.js"
 dotenv.config();
 
 const app = express();
+const corsOptions = {
+    origin:"http://localhost:5173",
+    // credentials:true
+}
+app.use(cors(corsOptions));
+// app.use(cors());
 
 // express middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-const corsOptions = {
-    origin:"http://localhost:5173",
-    credentials:true
-}
-app.use(cors(corsOptions));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    connectDB()
+    console.log(`Server running on ${PORT}`)
+})
+
 
 // APIs
 app.use("/api/v1/user",userRoute);
@@ -28,8 +35,4 @@ app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    connectDB()
-    console.log(`Server running on ${PORT}`)
-})
+
